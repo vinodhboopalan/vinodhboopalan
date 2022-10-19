002ec59514307ec00567f3a3c393efa9a2bdb202
Vinodh Boopalan <vinodh.boopalan@gmail.com>
2022-10-19 18:24:41 -0500

    Header VB

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Index() {
   return (
     <div>
-      <h1> Index </h1>
+      <h1> Vinodh Boopalan </h1>
       <Link href="/about">
         <a> About </a>
       </Link>
